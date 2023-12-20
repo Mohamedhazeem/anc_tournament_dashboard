7c0925b4b75084aa55a4c1c1a3c2597b575add60
Mohamed Hazeem <a.mohamedhazeem@gmail.com>
2023-12-20 18:36:51 +0530

    App and Style

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,29 @@
+import { useEffect, useState } from "react";
+import {
+  TOURNAMENT_URL,
+  fetchDetails,
+  fetchTournamentDetailType,
+} from "./utils/fetch";
+import GameComponent from "./components/GameComponent";
+
 function App() {
-  return <></>;
+  const [details, setDetails] = useState<fetchTournamentDetailType[]>();
+  useEffect(() => {
+    const detail = fetchDetails(TOURNAMENT_URL);
+    detail
+      .then((detail) => setDetails(detail))
+      .catch((err) => console.error(err));
+  }, []);
+
+  return (
+    <>
+      <div className="tournament-container">
+        {details?.map((detail) => (
+          <GameComponent game={detail.game} teams={detail.teams} />
+        ))}
+      </div>
+    </>
+  );
 }
 
 export default App;
